29ffc21a2903a596e7e5112ffb5504a1d447d7f8
jininono <jininono96@gmail.com>
2023-03-02 12:56:55 +0900

    aos수정

--- js/featured.js
+++ js/featured.js
@@ -54,42 +54,36 @@ $(function(){
 
 	}).resize();
 
-	//scroll
-	
-	window.addEventListener('resize', function(){
-	
-		if (window.innerWidth > 1023) {
-			window.addEventListener("wheel", function(e){
-				e.preventDefault();
-			},{passive : false});
-	
-			var $html = $("html");
-		 
-			var page = 1;
-			 
-			var lastPage = $("section").length + 1;
-			 
-			$html.animate({scrollTop:0},10);
-	
-			$(window).on("wheel", function(e){
-		 
-				if($html.is(":animated")) return;
-			 
-				if(e.originalEvent.deltaY > 0){
-					if(page== lastPage) return;
-			 
-					page++;
-				}else if(e.originalEvent.deltaY < 0){
-					if(page == 1) return;
-			 
-					page--;
-				}
-				var posTop = (page-1) * $(window).height();
-			 
-				$html.animate({scrollTop : posTop});
-			 
-			});
-		} 
+	//wheel
+	window.addEventListener("wheel", function(e){
+		e.preventDefault();
+	},{passive : false});
+
+	var $html = $("html");
+ 
+	var page = 1;
+	 
+	var lastPage = $("section").length + 1;
+	 
+	$html.animate({scrollTop:0},10);
+
+	$(window).on("wheel", function(e){
+ 
+		if($html.is(":animated")) return;
+	 
+		if(e.originalEvent.deltaY > 0){
+			if(page== lastPage) return;
+	 
+			page++;
+		}else if(e.originalEvent.deltaY < 0){
+			if(page == 1) return;
+	 
+			page--;
+		}
+		var posTop = (page-1) * $(window).height();
+	 
+		$html.animate({scrollTop : posTop});
+	 
 	});
 	
 });
